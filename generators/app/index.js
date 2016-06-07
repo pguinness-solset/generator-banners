c94ccdac708e2ea84e151741a283f57fc1dcaa20
pguinness-solset <patrick.guinness@solutionset.com>
2016-06-07 02:28:15 -0700

    Added templates and build script

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,32 +5,102 @@ var yosay = require('yosay');
 
 module.exports = yeoman.Base.extend({
   prompting: function () {
-    // Have Yeoman greet the user.
+    
+    var done = this.async();
+    var greeting = 'This action will create a scaffold for HTML5 banner projects.';
+
     this.log(yosay(
-      'Welcome to the astounding ' + chalk.red('generator-banners') + ' generator!'
+      'This action will create a scaffold for ' + chalk.blue('HTML5 banner projects') + '.'
     ));
 
     var prompts = [{
+      type: 'input',
+      name: 'campaignName',
+      message: 'Enter the name of this campaign:'
+    },{
+      type: 'input',
+      name: 'sizes',
+      message: 'Enter the banner sizes you would like to create as a comma-separated list (728x90, 300x250, 160x600):'
+    },{
       type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
+      name: 'includeEnabler',
+      message: 'Would you like to include Enabler?',
       default: true
     }];
 
-    return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+    return this.prompt(prompts).then(function (answers) {
+
+      var self = this;
+      var tmpSizes = answers.sizes.replace(/\s/g, '').split(',');
+      var numSizes = tmpSizes.length;
+      var sizes = [];
+
+      tmpSizes.forEach(function(value){
+        var dimensions = value.split('x');
+
+        if (!dimensions[0] || !dimensions[1]) {
+          return self.env.error(chalk.red('Invalid dimensions entered: ' + value));
+        }
+
+        var width = Math.floor(dimensions[0]).toString();
+        var height = Math.floor(dimensions[1]).toString();
+
+        if (width !== dimensions[0]) {
+          console.log('Width', width, 'Dimension', dimensions[0]);
+          return self.env.error(chalk.red('Invalid width entered in the following banner size: ' + value));
+        }
+
+        if (height !== dimensions[1]) {
+          console.log('Height', height, 'Dimension', dimensions[1]);
+          return self.env.error(chalk.red('Invalid height entered in the following banner size: ' + value));
+        }
+
+        sizes.push([width, height]);
+      });
+
+      this.props = {};
+      this.props.sizes = sizes;
+      this.props.campaignName = answers.campaignName;
+      this.props.includeEnabler = answers.includeEnabler;
+
+      done();
     }.bind(this));
   },
 
   writing: function () {
+
+    var self = this;
+
     this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+      this.templatePath('_package.json'),
+      this.destinationPath('package.json')
     );
+
+    this.fs.copy(
+      this.templatePath('gulpfile.js'),
+      this.destinationPath('gulpfile.js')
+    );
+
+    this.props.sizes.forEach(function(value){
+
+      var width = value[0];
+      var height = value[1];
+      var fileName = width + 'x' + height + '.html';
+
+      self.fs.copyTpl(
+        self.templatePath('index.html'),
+        self.destinationPath(fileName),
+        {
+          width: width,
+          height: height,
+          campaignName: self.props.campaignName,
+          includeEnabler: self.props.includeEnabler
+        }
+      );
+    });
   },
 
   install: function () {
-    this.installDependencies();
+    this.npmInstall();
   }
 });
